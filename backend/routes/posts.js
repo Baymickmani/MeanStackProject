556840614e557043e8f4eb43812fdf38d973ce8e
Baymickmani <chbmsivanagasai@gmail.com>
2020-07-16 20:04:07 +0530

    Backend with images

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,31 +1,70 @@
 const express = require('express');
+const multer = require('multer');
 
 const Post = require('../models/post');
 
 const router = express.Router();
 
-router.post('', (req, res, next) => {
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg'
+}
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error('Invalid Mime Type');
+        if (isValid) {
+            error = null;
+        }
+        cb(error, "backend/images");
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + '-' + Date.now() + '.' + "jpg");
+    }
+})
+
+router.post('', multer({storage: storage}).single("image"), (req, res, next) => {
+    const url = req.protocol + '://' + req.get("host");
     const post = new Post({
         title: req.body.title,
-        content: req.body.content
+        content: req.body.content,
+        imagePath: url + "/images/" + req.file.filename
     });
     post.save().then(result => {
         res.status(201).json({
             message: 'Post Created',
-            postId: result._id
+            post: {
+                ...result,
+                id: result._id
+            }
         })
     })
     
 })
 
 router.get('', (req, res, next) => {
-    Post.find()
-        .then(documents => {
-            res.status(200).json({
-                message: "Posts Fetched",
-                posts: documents
-            })
+    const pageSize = +req.query.pageSize;
+    const currentPage = +req.query.currentPage;
+    const postQuery = Post.find();
+    let fetchedPosts;
+    if(pageSize && currentPage) {
+        postQuery.skip(pageSize * (currentPage - 1))
+                 .limit(pageSize)
+    }
+    postQuery.then(documents => {
+        fetchedPosts = documents;
+        return Post.count();
+    })
+    .then(count => {
+        res.status(200).json({
+            message: "Posts Fetched",
+            posts: fetchedPosts,
+            maxPosts: count
         })
+    })
 })
 
 router.get('/:id', (req, res, next) => {
@@ -41,15 +80,20 @@ router.get('/:id', (req, res, next) => {
         })
 })
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id",  multer({storage: storage}).single("image"), (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if(req.file) {
+        const url = req.protocol + '://' + req.get('host');
+        imagePath = url + '/images/' + req.file.filename;
+    }
     const post = new Post({
         _id: req.body.id,
         title: req.body.title,
-        content: req.body.content
+        content: req.body.content,
+        imagePath: imagePath
     })
     Post.updateOne({_id: req.params.id }, post)
         .then(updatedPost => {
-            console.log(updatedPost)
             res.status(200).json({
                 message: "Update Success"
             })
